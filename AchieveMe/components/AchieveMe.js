ddc08062e7aa1bb9afb6916887bf23689231b2bd
Joern Bernhardt <jb@campudus.com>
2017-06-05 20:30:12 +0200

    Add achievement route
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- AchieveMe/components/AchieveMe.js
+++ AchieveMe/components/AchieveMe.js
@@ -1,13 +1,14 @@
 import React from "react";
 import {StyleSheet, Text, View} from 'react-native';
 import {NativeRouter, Route} from "react-router-native";
-import {Home, First, Second} from "./index";
+import {Achievements, Home, First, Second} from "./index";
 
 export const AchieveMe = ({children}) => {
   return (
     <NativeRouter>
       <View style={styles.container}>
         <Route path="/" exact component={Home} styles={styles.welcome} />
+        <Route path="/achievements" component={Achievements} styles={styles.welcome} />
         <Route path="/first" component={First} styles={styles.welcome} />
         <Route path="/second" component={Second} styles={styles.welcome} />
         <Text style={styles.instructions}>
--- AchieveMe/components/index.js
+++ AchieveMe/components/index.js
@@ -1,4 +1,5 @@
 export {AchieveMe} from "./AchieveMe";
+export {Achievements} from "./Achievements";
 export {Home} from "./Home";
 export {First} from "./First";
 export {Second} from "./Second";
