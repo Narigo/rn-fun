51b2921da34930cf14f7f5037651b9dff82fcd63
Joern Bernhardt <jb@campudus.com>
2017-06-10 22:54:52 +0200

    Add link to Home screen
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- AchieveMe/components/Splash.js
+++ AchieveMe/components/Splash.js
@@ -1,15 +1,18 @@
 import React from "react";
 import {StyleSheet, Text, View} from "react-native";
+import {Link} from "react-router-native";
 
 export const Splash = () => (
-  <View style={styles.screen}>
-    <Text style={styles.logo}>Logo</Text>
-    <Text style={styles.title}>
-      <Text style={styles.titleFirst}>achieve</Text>
-      <Text style={styles.titleSecond}>me</Text>
-    </Text>
-    <Text style={styles.small}>honor feels good.</Text>
-  </View>
+  <Link to="/achieveme/">
+    <View style={styles.screen}>
+      <Text style={styles.logo}>Logo</Text>
+      <Text style={styles.title}>
+        <Text style={styles.titleFirst}>achieve</Text>
+        <Text style={styles.titleSecond}>me</Text>
+      </Text>
+      <Text style={styles.small}>honor feels good.</Text>
+    </View>
+  </Link>
 );
 
 const styles = StyleSheet.create({
