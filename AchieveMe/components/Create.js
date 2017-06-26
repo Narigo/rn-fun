58a58f02eaedac0d40e26d5a56cbc1c3ca92c750
Joern Bernhardt <jb@campudus.com>
2017-06-26 22:11:07 +0200

    Add example to overwrite some styling
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- AchieveMe/components/Create.js
+++ AchieveMe/components/Create.js
@@ -1,12 +1,18 @@
 import React from "react";
-import {ScrollView} from "react-native";
+import {ScrollView, StyleSheet} from "react-native";
 import {Text} from "./Text";
 import {Link} from "react-router-native";
 
 export const Create = () => {
   return (
     <ScrollView>
-      <Link to="/list"><Text>Back to Home</Text></Link>
+      <Link to="/list"><Text style={styles.bigger}>Back to Home</Text></Link>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  bigger: {
+    fontSize: 24
+  }
+});
