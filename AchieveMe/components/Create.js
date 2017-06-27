72f1aba52e06f56680d9b78e7d600e9ffda12eba
Joern Bernhardt <jb@campudus.com>
2017-06-27 10:28:35 +0200

    Use multiple views in Create component
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- AchieveMe/components/Create.js
+++ AchieveMe/components/Create.js
@@ -1,13 +1,44 @@
 import React from "react";
-import {ScrollView, StyleSheet} from "react-native";
+import {View, ScrollView, StyleSheet} from "react-native";
 import {Text} from "./Text";
 import {Link} from "react-router-native";
 
 export const Create = () => {
   return (
-    <ScrollView>
-      <Link to="/list"><Text style={styles.bigger}>Back to Home</Text></Link>
-    </ScrollView>
+    <View>
+      <View>
+        <Text>Logo</Text>
+        <Link to="/list"><Text>Back</Text></Link>
+      </View>
+      <ScrollView>
+        <Text>Friend A</Text>
+        <Text>Friend B</Text>
+        <Text>Friend C</Text>
+        <Text>Friend D</Text>
+        <Text>Friend E</Text>
+        <Text>Friend F</Text>
+        <Text>Friend G</Text>
+        <Text>Friend H</Text>
+        <Text>Friend I</Text>
+        <Text>Friend J</Text>
+        <Text>Friend K</Text>
+        <Text>Friend L</Text>
+        <Text>Friend M</Text>
+        <Text>Friend N</Text>
+        <Text>Friend O</Text>
+        <Text>Friend P</Text>
+        <Text>Friend Q</Text>
+        <Text>Friend R</Text>
+        <Text>Friend S</Text>
+        <Text>Friend T</Text>
+        <Text>Friend U</Text>
+        <Text>Friend V</Text>
+        <Text>Friend W</Text>
+        <Text>Friend X</Text>
+        <Text>Friend Y</Text>
+        <Text>Friend Z</Text>
+      </ScrollView>
+    </View>
   );
 };
 
