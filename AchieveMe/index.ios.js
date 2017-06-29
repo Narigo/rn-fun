0d0cd7e0dfd2e9643d52986151606afe1a84ce8f
Joern Bernhardt <jb@campudus.com>
2017-06-29 06:53:07 +0200

    Remove unnecessary App and just use AchieveMe
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- AchieveMe/index.ios.js
+++ AchieveMe/index.ios.js
@@ -2,12 +2,4 @@ import React, {Component} from "react";
 import {AppRegistry} from "react-native";
 import {AchieveMe} from "./components/index";
 
-export default class App extends Component {
-  render() {
-    return (
-      <AchieveMe/>
-    );
-  }
-}
-
-AppRegistry.registerComponent("AchieveMe", () => App);
+AppRegistry.registerComponent("AchieveMe", () => AchieveMe);
