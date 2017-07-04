7059cfbdba2fa42e5463fe632f030a2dec5cf0f2
Joern Bernhardt <jb@campudus.com>
2017-07-04 21:38:52 +0200

    Style text a bit with inline styles
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- AchieveMe/components/Achievements.js
+++ AchieveMe/components/Achievements.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {ScrollView} from "react-native";
+import {ScrollView, StyleSheet} from "react-native";
 import {Achievement, Text} from "./";
 import {Link} from "react-router-native";
 
@@ -7,7 +7,23 @@ export const Achievements = ({achievements}) => {
   return (
     <ScrollView>
       {achievements.map((achievement, idx) => <Achievement key={idx} {...achievement} />)}
-      <Link to="/home"><Text>Back to Home</Text></Link>
+      <Link to="/home" style={styles.linkWrap}><Text style={styles.wrap}><Text style={styles.inner}>Back to Home</Text></Text></Link>
     </ScrollView>
   );
 };
+
+const styles = StyleSheet.create({
+  linkWrap: {
+    flex: 1,
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "center",
+  },
+  wrap: {
+    backgroundColor: "#0f0",
+    margin: 5
+  },
+  inner: {
+    color: "#000"
+  }
+});
