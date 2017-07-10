ba2c4b45fbb10146e8388b612985bcfba9f9f246
Joern Bernhardt <jb@campudus.com>
2017-07-10 08:52:02 +0200

    Begin styling of score
    Signed-off-by: Joern Bernhardt <jb@campudus.com>

--- AchieveMe/components/Achievement.js
+++ AchieveMe/components/Achievement.js
@@ -15,7 +15,11 @@ export const Achievement = ({awardedBy, details, name, score, setAchievement}) =
     return (
       <View style={styles.inactive}>
         <TouchableOpacity onPress={setAchievement}>
-          <Text style={styles.score}>{score}</Text>
+          <View style={styles.scoreContainer}>
+            <Text style={styles.leftScore}>-</Text>
+            <Text style={styles.score}>{score}</Text>
+            <Text style={styles.rightScore}>-</Text>
+          </View>
           <Text style={styles.name}>{name}</Text>
         </TouchableOpacity>
       </View>
@@ -45,6 +49,20 @@ const styles = StyleSheet.create({
   awardedBy: {
     fontSize: 10,
     color: "gold"
+  },
+  scoreContainer: {
+    flex: 1,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center"
+  },
+  leftScore: {
+    borderTopWidth: 5,
+    borderTopColor: "#aaa",
+  },
+  rightScore: {
+    borderTopWidth: 5,
+    borderColor: "#aaa",
   },
   score: {
     fontSize: 8,
